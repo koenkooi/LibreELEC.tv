c16d08cff1790f8c7bddbc57cfb7f9c675a78ede
kszaq <kszaquitto@gmail.com>
2016-08-24 17:45:23 +0200

    packages/multimedia/libamcodec: update to latest buildroot version (2016-08-18)
    Thanks @surkovalex

--- packages/multimedia/libamcodec/package.mk
+++ packages/multimedia/libamcodec/package.mk
@@ -27,8 +27,8 @@ case $TARGET_ARCH in
     PKG_URL="https://github.com/codesnake/libamcodec/archive/$PKG_VERSION.tar.gz"
     ;;
   aarch64)
-		PKG_VERSION="2fba80c"
-		PKG_URL="https://github.com/surkovalex/libamcodec/archive/$PKG_VERSION.tar.gz"
+    PKG_VERSION="2fba80c"
+    PKG_URL="https://github.com/surkovalex/libamcodec/archive/$PKG_VERSION.tar.gz"
     ;;
 esac
 PKG_DEPENDS_TARGET="toolchain"
